244773b7c6d41c8b460bd27cd6acd02783e1525a
saifur rahman <saifuroracle@gmail.com>
2022-06-12 23:49:50 +0600

    updated

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const http = require('http');
 const server = http.createServer(app);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+    // res.send('<h1>Hello world</h1>');
+    res.sendFile('./app/views/index.html');
 });
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
+
