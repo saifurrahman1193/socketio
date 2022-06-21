ab88c6f53fa79a93a6d278899bbaf33f5de56772
saifur rahman <saifuroracle@gmail.com>
2022-06-21 19:51:39 +0600

    project updated

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
 const http = require('http');
+const app = express();
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 const bodyParser = require("body-parser");
+const cors = require("cors");
+var multer = require('multer');
+var forms = multer();
+
+
+// =========cors==========
+app.use(cors());
+// =========cors==========
+
+// parse requests of content-type: application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse requests of content-type: application/json
+app.use(bodyParser.json());
+
+// for parsing multipart/form-data
+app.use(forms.array()); 
+app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
     // res.send('<h1>Hello world</h1>');
