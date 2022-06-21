c99af2a7d10584cd686ccd938171ac3639bf38f0
saifur rahman <saifuroracle@gmail.com>
2022-06-22 00:20:21 +0600

    project structure

--- app/exceptions/handler.js
+++ app/exceptions/handler.js
@@ -1,24 +1,28 @@
 module.exports = (app, path, loghelper) => {
 
     // catch 404 and forward to error handler
-    app.use(function(req, res, next){
+    app.use(function (req, res, next) {
 
-        let options = { root: path.join(__dirname, '../../resources/views/errors') }
-    
+        let options = {
+            root: path.join(__dirname, '../../resources/views/errors')
+        }
+
         res.status(404);
-    
+
         // respond with html page
         if (req.accepts('html')) {
-        res.sendFile('404.html', options);
-        return;
+            res.sendFile('404.html', options);
+            return;
         }
-    
+
         // respond with json
         if (req.accepts('json')) {
-        res.send({ error: 'Not found' });
-        return;
+            res.send({
+                error: 'Not found'
+            });
+            return;
         }
-    
+
         // default to plain-text. send()
         res.type('txt').send('Not found');
     });
@@ -26,15 +30,15 @@ module.exports = (app, path, loghelper) => {
 
 
     // Handling Errors
-    app.use( async(err, req, res, next) => {
+    app.use(async (err, req, res, next) => {
         // console.log(err);
         err.statusCode = err.statusCode || 500;
         err.message = err.message || "Internal Server Error";
-    
+
         await loghelper.log(error?.message, 'error')
-    
+
         res.status(err.statusCode).json({
-        message: err.message,
+            message: err.message,
         });
     });
 
