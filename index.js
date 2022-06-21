eaedeb941120549ff0d718eed8029f41100ca592
saifur rahman <saifuroracle@gmail.com>
2022-06-21 19:22:06 +0600

    app

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const bodyParser = require("body-parser");
 
 app.get('/', (req, res) => {
     // res.send('<h1>Hello world</h1>');
