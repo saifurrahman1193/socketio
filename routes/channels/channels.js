c6d98a99a1c2e6e60eccc2f673b1b08b2656e98d
saifur rahman <saifuroracle@gmail.com>
2022-07-05 00:06:57 +0600

    socket app

--- routes/channels/channels.js
+++ routes/channels/channels.js
@@ -7,18 +7,11 @@ module.exports = (app, expressServer) => {
     const io = new Server(expressServer);
 
 
-    let buyNsp = io.of("/buy")
-
-    buyNsp.on('connection', (socket) => {
-        console.log('New user connected ', moment().format('YYYY-MM-DD hh:mm:ss a'));
-        buyNsp.emit("buyNspMessage", "This is buyNsp message")
-    });
-
-    let sellNsp = io.of("/sell")
-
-    sellNsp.on('connection', (socket) => {
-        console.log('New user connected ', moment().format('YYYY-MM-DD hh:mm:ss a'));
-        buyNsp.emit("sellNspMessage", "This is sellNsp message")
-    });
+    io.on('connection', (socket) => {
+        console.log('A user is connected');
+        socket.on('chat', (msg) =>{
+            socket.emit('chat_message',msg)
+        })
+    })
 
 };
